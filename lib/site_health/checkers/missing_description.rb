dda4a12476493ec43363c9d325e733886733634c
Jacob Burenstam <burenstam@gmail.com>
2018-08-26 15:21:46 +0200

    Add issues in MissingDescription checker

--- lib/site_health/checkers/missing_description.rb
+++ lib/site_health/checkers/missing_description.rb
@@ -10,14 +10,23 @@ module SiteHealth
       # @return [Boolean] determines whether the description is missing
       return add_data(missing: false) if page.redirect?
 
-      page.search('//meta').map do |meta|
+      page.search('//meta').each do |meta|
         name = (meta.attributes['name'] || meta.attributes['http-equiv']).to_s.strip
         next unless name == 'description'
 
-        return add_data(missing: meta.attributes['content'].to_s.strip.empty?)
+        missing = meta.attributes['content'].to_s.strip.empty?
+        return add_missing_issue_and_data if missing
+        return add_data(missing: false)
       end
 
+      add_missing_issue_and_data
+    end
+
+    private
+
+    def add_missing_issue_and_data
       add_data(missing: true)
+      add_issue(title: 'missing description', severity: :medium, priority: :high)
     end
   end
 
